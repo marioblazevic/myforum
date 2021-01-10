77693f05ece275b0ecd0562b770ddbfb8feb9f36
unknown <blazevicmario97@gmail.com>
2021-01-10 22:09:26 +0100

    Edited forum

--- resources/views/components/links.blade.php
+++ resources/views/components/links.blade.php
@@ -1,7 +1,7 @@
 <div class="links mb-4 d-flex justify-content-between">
-    <a href="{{ route('posts') }}" class="btn btn-light btn-md active flex-1 mr-1" role="button" aria-pressed="true">Home</a>
-    <a href="{{ route('profile', auth()->user()) }}" class="btn btn-light btn-md active flex-1 mr-1" role="button" aria-pressed="true">Profile</a>
-    <a href="{{ route('following-users') }}" class="btn btn-light btn-md active flex-1 mr-1" role="button" aria-pressed="true">Following</a>
-    <a href="{{ route('create-post') }}" class="btn btn-light btn-md active flex-1 mr-1" role="button" aria-pressed="true">Create Post</a>
-    <a href="{{ route('explore-users') }}" class="btn btn-light btn-md active flex-1 mr-1" role="button" aria-pressed="true">Explore</a>
+    <a href="{{ route('posts') }}" class="btn btn-md active flex-1 mr-1 {{ Request::path() === 'posts' ? 'btn-primary' : 'btn-light' }}" role="button" aria-pressed="true">Home</a>
+    <a href="{{ route('profile', auth()->user()) }}" class="btn  btn-md active flex-1 mr-1 {{ Request::is('profile/*') ? 'btn-primary' : 'btn-light' }}" role="button" aria-pressed="true">Profile</a>
+    <a href="{{ route('following-users') }}" class="btn  btn-md active flex-1 mr-1 {{ Request::path() === 'following' ? 'btn-primary' : 'btn-light' }}" role="button" aria-pressed="true">Following</a>
+    <a href="{{ route('create-post') }}" class="btn btn-md active flex-1 mr-1 {{ Request::is('posts/create') ? 'btn-primary' : 'btn-light' }}" role="button" aria-pressed="true">Create Post</a>
+    <a href="{{ route('explore-users') }}" class="btn btn-md active flex-1 mr-1 {{ Request::path() === 'explore' ? 'btn-primary' : 'btn-light' }}" role="button" aria-pressed="true">Explore</a>
 </div>
